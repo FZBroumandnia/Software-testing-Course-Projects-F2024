477bcb20625c8a072c4bd6b429adc8e998db7bd0
Armi-B <armitabahrudi3@gmail.com>
2024-10-18 14:53:05 +0330

    Add 4 Test Class

---
+++ Mizdooni/src/test/java/mizdooni/model/RatingTest.java
@@ -0,0 +1,15 @@
+package mizdooni.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RatingTest {
+
+    @Test
+    public void getStarCountTest()
+    {
+        fail("Not yet implemented");
+    }
+
+
+}
---
+++ Mizdooni/src/test/java/mizdooni/model/TableTest.java
@@ -0,0 +1,7 @@
+package mizdooni.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TableTest {
+}
