6c62d09384806b8d2dc49167d28f704de9d10598
Armi-B <armitabahrudi3@gmail.com>
2024-11-01 00:56:33 +0330

    add more test in that class

--- Mizdooni/src/test/java/mizdooni/controllers/ReviewControllerTest.java
+++ Mizdooni/src/test/java/mizdooni/controllers/ReviewControllerTest.java
@@ -13,14 +13,12 @@ import org.mockito.InjectMocks;
 
 import static org.mockito.Mockito.*;
 
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertTrue;
@@ -55,6 +53,21 @@ public class ReviewControllerTest {
         return 9;
     }
 
+    Rating make_a_rating() {
+        Rating r = new Rating();
+        r.service = 1;
+        r.food = 1;
+        r.ambiance = 1;
+        r.overall = 1;
+        return  r;
+    }
+
+    void stub_set_up_existing_restaurant() {
+        lenient().when(ExistingRestaurant.getName()).thenReturn("existing restaurant");
+        when(ExistingRestaurant.getId()).thenReturn(existing_restaurant_id());
+        when(restaurantService.getRestaurant(existing_restaurant_id())).thenReturn(ExistingRestaurant);
+    }
+
     @Test
     void getReviews_When_NonExistingRestaurant_Then_NotFound() {
         ResponseException exception = assertThrows(ResponseException.class, () -> {
@@ -68,11 +81,9 @@ public class ReviewControllerTest {
     }
 
     @Test
-    void getReviews_When_NonExistingReviewForRestaurant_Null() {
+    void getReviews_When_NonExistingReviewForRestaurant_Then_Null() {
         try {
-            when(ExistingRestaurant.getName()).thenReturn("existing restaurant");
-            when(ExistingRestaurant.getId()).thenReturn(existing_restaurant_id());
-            when(restaurantService.getRestaurant(existing_restaurant_id())).thenReturn(ExistingRestaurant);
+            stub_set_up_existing_restaurant();
             when(reviewService.getReviews(anyInt(), anyInt())).thenReturn(null);
             Response r = reviewController.getReviews(existing_restaurant_id(), DEFAULT_PAGE_NUM);
 
@@ -86,12 +97,10 @@ public class ReviewControllerTest {
     }
 
     @Test
-    void getReviews_When_ExistingReview_Match() {
+    void getReviews_When_ExistingReview_Then_success() {
         try {
             PagedList<Review> pagedList = new PagedList<>(new ArrayList<>(List.of(mockReview)), DEFAULT_PAGE_NUM, 10);
-            when(ExistingRestaurant.getName()).thenReturn("existing restaurant");
-            when(ExistingRestaurant.getId()).thenReturn(existing_restaurant_id());
-            when(restaurantService.getRestaurant(existing_restaurant_id())).thenReturn(ExistingRestaurant);
+            stub_set_up_existing_restaurant();
             when(reviewService.getReviews(existing_restaurant_id(), DEFAULT_PAGE_NUM)).thenReturn(pagedList);
             Response response = reviewController.getReviews(existing_restaurant_id(), DEFAULT_PAGE_NUM);
 
@@ -105,4 +114,58 @@ public class ReviewControllerTest {
             fail("Unexpected RestaurantNotFound exception thrown");
         }
     }
+
+    @Test
+    void addReviews_When_ExistingRestaurant_Then_success() {
+        stub_set_up_existing_restaurant();
+        Map<String, Object> ratingMap = Map.of("comment", "comment!", "rating", Map.of(
+                "food", 1,
+                "service", 1,
+                "ambiance", 1,
+                "overall", 1
+        ));
+        Response response = reviewController.addReview(existing_restaurant_id(), ratingMap);
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("review added successfully", response.getMessage());
+        assertEquals(true, response.isSuccess());
+    }
+
+//    @Test
+//    void testAddReview_UserNotFound() {
+//        stub_set_up_existing_restaurant();
+//        Map<String, Object> ratingMap = Map.of(
+//                "comment", "comment!",
+//                "rating", Map.of(
+//                        "food", 1,
+//                        "service", 1,
+//                        "ambiance", 1,
+//                        "overall", 1
+//                )
+//        );
+//        try {
+//
+//            when(reviewService.addReview(anyInt(), any(Rating.class), anyString())).thenThrow(new UserNotFound());
+//            reviewController.addReview(existing_restaurant_id(), ratingMap);
+//            fail("Expected UserNotFound to be thrown");
+//        } catch (UserNotFound exception) {
+//            assertEquals(new UserNotFound(), exception);
+//        }
+//        catch (ManagerCannotReview o){
+//            fail("Expected UserNotFound to be thrown");
+//        }
+//        catch (RestaurantNotFound o)
+//        {
+//            fail("Expected UserNotFound to be thrown");
+//        }
+//        catch (InvalidReviewRating o)
+//        {
+//            fail("Expected UserNotFound to be thrown");
+//        }
+//        catch (UserHasNotReserved o)
+//        {
+//            fail("Expected UserNotFound to be thrown");
+//        }
+//        verify(reviewService).addReview(anyInt(), any(Rating.class), anyString());
+//    }
 }
